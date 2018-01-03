5c6398ea7efd8df9f51af150c3afa8b6cbc9d46b
Vardan Margaryan <v.t.margaryan@gmail.com>
2018-01-03 20:28:02 +0300

    Completed S10.02

--- S10.02-Exercise-SmarterSyncing/app/src/main/java/com/example/android/sunshine/sync/SunshineSyncUtils.java
+++ S10.02-Exercise-SmarterSyncing/app/src/main/java/com/example/android/sunshine/sync/SunshineSyncUtils.java
@@ -15,20 +15,50 @@
  */
 package com.example.android.sunshine.sync;
 
+import android.content.ContentProvider;
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.os.AsyncTask;
 import android.support.annotation.NonNull;
+
+import com.example.android.sunshine.data.WeatherContract;
 
 
 public class SunshineSyncUtils {
 
-//  TODO (1) Declare a private static boolean field called sInitialized
+//  DONE (1) Declare a private static boolean field called sInitialized
+    private static boolean sInitialized;
 
-    //  TODO (2) Create a synchronized public static void method called initialize
-    //  TODO (3) Only execute this method body if sInitialized is false
-    //  TODO (4) If the method body is executed, set sInitialized to true
-    //  TODO (5) Check to see if our weather ContentProvider is empty
-        //  TODO (6) If it is empty or we have a null Cursor, sync the weather now!
+    //  DONE (2) Create a synchronized public static void method called initialize
+    synchronized public static void initialize(final Context context) {
+        //  DONE (3) Only execute this method body if sInitialized is false
+        if (sInitialized) return;
+        //  DONE (4) If the method body is executed, set sInitialized to true
+        sInitialized = true;
+
+        //  DONE (5) Check to see if our weather ContentProvider is empty
+        new AsyncTask() {
+            Cursor mCursor;
+            @Override
+            protected Object doInBackground(Object[] objects) {
+                mCursor = context.getContentResolver().query(WeatherContract.WeatherEntry.CONTENT_URI,
+                        null,
+                        null,
+                        null,
+                        null);
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Object o) {
+                super.onPostExecute(o);
+                if (mCursor == null || mCursor.getCount() == 0) {
+                    startImmediateSync(context);
+                }
+            }
+        }.execute();
+    }
 
     /**
      * Helper method to perform a sync immediately using an IntentService for asynchronous
